ea9aef5d68484f6e47f0fbda41f708c848d6f6d6
eniac314 <florian.gillard@tutanota.com>
2025-02-12 09:20:42 +0100

    added pkg-js

---
+++ elm-pkg-js/coms.js
@@ -0,0 +1,30 @@
+exports.init = function(app) {
+    window.parent.postMessage("kgg-loaded", "*");
+
+    window.addEventListener("message", (event) => {
+        console.log("Message from host:", event.data);
+    });
+
+    // app.ports.getGeoloc.subscribe(async function() {
+    //     getLocation(app)
+    // })
+}
+
+// function getLocation(app) {
+//     var options = {
+//         enableHighAccuracy : true,
+//         timeout : 5000,
+//         maximumAge : 0
+//     };
+
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(app.ports.geoloc.send,
+//             function(error) {
+//                 app.ports.geoloc.send("geolocation not allowed");
+//             },
+//             options);
+//     } else {
+//         app.ports.geoloc.send("no geolocation");
+//         console.log("no geolocation");
+//     }
+// }
